29fa0ddd9ce642e9d551e19ecae2f70728e98841
Vikas Cheruku <vikcher@Vikass-MacBook-Pro.local>
2016-08-14 13:56:06 -0400

    Fixed navController error

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -17,7 +17,7 @@
 		});
 	}]);
 	
-	app.controller('navController'), function() {
+	app.controller('navController', function() {
 		this.tab = 1;
 		
 		this.selectTab = function(setTab) {
@@ -27,5 +27,5 @@
 		this.isSelected = function(checkTab) {
 			return this.tab === checkTab;
 		};
-	};
+	});
 })();
