2b2c9fae746b81662965813dbe60e84751a65ec6
Vikas Cheruku <vikcher@Vikass-MacBook-Pro.local>
2016-08-13 21:18:55 -0400

    Testing list products with REST API

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,4 +2,18 @@
  * 
  */
 
-var app = angular.module('shoppingcart', []);
+(function() {
+	var app = angular.module('shoppingcart', []);
+	
+	app.controller('storeController', ['$http', function($http) {
+		var store = this;
+		
+		store.products = [];
+		
+		$http.get({method : 'GET', url : '/rest/products'}).then(function successCallback(response){
+			store.products = response.products;
+		}, function errorCallBack(response) {
+			
+		});
+	}]);
+})();
