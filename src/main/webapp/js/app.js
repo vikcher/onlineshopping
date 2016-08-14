2433d334873ebaf2c8fa58f16c7688f78cead8af
Vikas Cheruku <vikcher@Vikass-MacBook-Pro.local>
2016-08-14 16:14:57 -0400

    Added directive for products

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -5,6 +5,7 @@
 (function() {
 	var app = angular.module('shoppingcart', []);
 	
+	/*
 	app.controller('productController', ['$http', function($http) {
 		var store = this;
 		store.products = [];
@@ -16,6 +17,7 @@
 			
 		});
 	}]);
+	*/
 	
 	app.controller('navController', function() {
 		this.tab = 1;
@@ -26,6 +28,25 @@
 		
 		this.isSelected = function(checkTab) {
 			return this.tab === checkTab;
+		};
+	});
+	
+	app.directive('productItem', function() {
+		return {
+			restrict: 'E',
+		    templateUrl : "product-item.html",
+		    controller : ['$http', function($http) {
+		    	var store = this;
+				store.products = [];
+				
+				$http({method : 'GET', url : "https://vast-everglades-25484.herokuapp.com/rest/products"}).then(function successCallback(response){
+					store.products = response.data.products;
+					console.log(response.data.products);
+				}, function errorCallBack(response) {
+					
+				});
+		    }],
+		    controllerAs : 'store'
 		};
 	});
 })();
