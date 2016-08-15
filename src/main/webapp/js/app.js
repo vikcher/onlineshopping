6375f96ad4451181ff975dd5a65b9b4cf9c5a2a2
Vikas Cheruku <vikcher@Vikass-MacBook-Pro.local>
2016-08-15 08:02:41 -0400

    Revamped design of products tab

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,38 +3,5 @@
  */
 
 (function() {
-	var app = angular.module('shoppingcart', ['navigation']);
-	
-	app.directive('productItem', function() {
-		return {
-			restrict: 'E',
-		    templateUrl : "product-item.html",
-		    controller : ['$http', function($http) {
-		    	this.formAdd = [];
-		    	var store = this;
-				store.products = [];
-				
-				$http({method : 'GET', url : "https://vast-everglades-25484.herokuapp.com/rest/products"}).then(function successCallback(response){
-					store.products = response.data.products;
-					console.log(response.data.products);
-				}, function errorCallBack(response) {
-					
-				});
-				
-				this.addToCart = function(id, size, color, quantity){
-					$http({
-						method : 'PUT',
-						url : "https://vast-everglades-25484.herokuapp.com/rest/cart/"+id+"?color=" + encodeURIComponent(color)
-						       + "&size=" + encodeURIComponent(size) + "&quantity=" +encodeURIComponent(quantity),
-						headers : {
-							'Content-Type' : 'application/x-www-form-urlencoded',
-							'Authorization' : 'Bearer o1pjjkuo8vhmha5bip1898top1'
-						}
-					}).then(function successCallBack(response){console.log(response)}, function errorCallBack(){});
-					this.formAdd[id] = {};
-				};
-		    }],
-		    controllerAs : 'store'
-		};
-	});
+	var app = angular.module('shoppingcart', ['navigation','products']);
 })();
