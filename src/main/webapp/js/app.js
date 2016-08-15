23d351d33b04c2c430943947e284e4b71c9c234e
Vikas Cheruku <vikcher@Vikass-MacBook-Pro.local>
2016-08-15 00:45:35 -0400

    Refactored nav controller

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,33 +3,7 @@
  */
 
 (function() {
-	var app = angular.module('shoppingcart', []);
-	
-	/*
-	app.controller('productController', ['$http', function($http) {
-		var store = this;
-		store.products = [];
-		
-		$http({method : 'GET', url : "https://vast-everglades-25484.herokuapp.com/rest/products"}).then(function successCallback(response){
-			store.products = response.data.products;
-			console.log(response.data.products);
-		}, function errorCallBack(response) {
-			
-		});
-	}]);
-	*/
-	
-	app.controller('navController', function() {
-		this.tab = 1;
-		
-		this.selectTab = function(setTab) {
-			this.tab = setTab;
-		};
-		
-		this.isSelected = function(checkTab) {
-			return this.tab === checkTab;
-		};
-	});
+	var app = angular.module('shoppingcart', ['navigation']);
 	
 	app.directive('productItem', function() {
 		return {
---
+++ src/main/webapp/js/nav.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+(function() {
+	var app = angular.module('navigation', []);
+	
+	app.controller('navController', function() {
+		this.tab = 1;
+		
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	});
+})();
